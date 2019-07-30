dbe797bc7dd557892d3d653dd6cd7c6657eda2c8
taisyo0000 <itou.hiroki.kyoto@gmail.com>
2019-07-30 21:00:18 +0900

    Initial Commit

---
+++ Tokkiki2Dcamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tokkiki2Dcamera
+//
+//  Created by 伊藤大生 on 2019/07/30.
+//  Copyright © 2019 ItoHiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
